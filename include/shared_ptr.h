a8b19cf7c59928088679fbdbfb0c4626da944a60
Sajad Ghadiri <cjanthony.romeo@gmail.com>
2022-05-05 13:09:18 +0000

    I implement use_count method and with some changes totally 21 tests passed :)

--- include/shared_ptr.h
+++ include/shared_ptr.h
@@ -6,22 +6,27 @@ template<typename T>
 class SharedPtr
 {
 public:
-
+    
     SharedPtr(T* N); // constructor
     SharedPtr(); // default constructor
     ~SharedPtr(); // destructor 
+    SharedPtr(const SharedPtr& ptr) ; // copy constructor
 
-    
+
     T& operator*(); // * operator
+    T* operator->(); // -> operator
+    SharedPtr<T>& operator=(const SharedPtr& ptr) ; // assignment operator
 
 
     T* get(); // get raw pointer stored in the class
     void reset(); // reset the pointer to nullptr
     T* reset(T* inp) ; // reset the pointer and make new pointer by input value
-
+    int use_count();   // get the number of shared pointers
+    explicit operator bool(); // bool operator
 
 private:
 T* _p ;
+int* number ;
 
 };
 
--- include/shared_ptr.hpp
+++ include/shared_ptr.hpp
@@ -4,7 +4,7 @@
 template<typename T>
 SharedPtr<T>::SharedPtr(T* N) : _p{N}
 {
-
+    number = new int(1) ;
 }
 
 ///////////////////////// default constructor ///////////////////////////
@@ -12,26 +12,59 @@ template<typename T>
 SharedPtr<T>::SharedPtr()
 {
     _p = nullptr ;
+    number = new int(0)  ;
+}
+/////////////////////// copy constructor ///////////////////////////////
+template<typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& ptr)
+{
+    _p = ptr._p ;
+    number = ptr.number  ;
+    *number = *number + 1 ;
 }
 /////////////////////// Destructor ///////////////////////////////
 template<typename T>
 SharedPtr<T>::~SharedPtr()
 {
-    delete _p ;
-    _p = nullptr ;
+    *number = *number - 1 ;
+
+    if(*number == 0)
+    {
+        delete _p ;
+        _p = nullptr ;
+    }
 }
-/////////////////////// make_shared ///////////////////////////////
+/////////////////////// make_shared //////////////////////////////////
 template<typename T>
 T* make_shared(T N)
 {
     return new T{N} ;
 }
-/////////////////////// operator* ///////////////////////////////
+/////////////////////// operator* ///////////////////////////////////
 template<typename T>
 T& SharedPtr<T>::operator*()
 {
     return *_p ;
 }
+/////////////////////// operator -> ///////////////////////////////
+template<typename T>
+T* SharedPtr<T>::operator->()
+{
+    return _p ;
+}
+/////////////////////// operator = //////////////////////////////////
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr<T>& ptr) 
+{
+    if( _p == ptr._p )
+        return *this ;
+
+    _p = ptr._p ;
+    number = ptr.number ;
+    *number = *number + 1 ;
+    return *this ;
+}
+
 /////////////////////// get /////////////////////////////////////////
 template<typename T>
 T* SharedPtr<T>::get()
@@ -43,6 +76,7 @@ template<typename T>
 void SharedPtr<T>::reset()
 {
     _p = nullptr ;
+    *number = 0 ;
 }
 /////////////////////// reset with input////////////////////////////////
 template<typename T>
@@ -50,5 +84,26 @@ T* SharedPtr<T>::reset(T* inp)
 {
     delete _p ;
     _p = inp ;
+    *number = 1 ;
     return _p ;
+}
+/////////////////////// use_count /////////////////////////////////////////
+template<typename T>
+int SharedPtr<T>::use_count()
+{
+    return *number ;
+}
+
+////////////////////////////bool operator ////////////////////////////////
+template<typename T>
+SharedPtr<T>::operator bool()
+{
+    if(_p == nullptr)
+    {
+        return false ;
+    }
+    else
+    {
+        return true ;
+    }
 }
